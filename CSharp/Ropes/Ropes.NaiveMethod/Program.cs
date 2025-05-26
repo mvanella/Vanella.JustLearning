71131916899ead1a2062386e6c6a303ed95636c0
Micah Vanella <micah.vanella@gmail.com>
2025-05-26 14:57:15 -0400

    Fixed up dotnet solution file and project structures. Added rope method projects.

---
+++ CSharp/Ropes/Ropes.NaiveMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Please specify two strings to concatenate.");
+            return;
+        }
+
+        char[] a = args[0].ToCharArray();
+        char[] b = args[1].ToCharArray();
+        int n1 = a.Length;
+        int n2 = b.Length;
+
+        char[] c = new char[n1 + n2];
+        Concatenate(a, b, c, n1, n2);
+
+        Console.WriteLine("First as an array: ");
+        for (int i = 0; i < n1 + n2; i++)
+        {
+            Console.Write(c[i]);
+        }
+        Console.Write(Environment.NewLine);
+
+        Console.WriteLine("Now as a string: ");
+        Console.WriteLine(c);
+
+        Console.WriteLine("Thanks, bye");
+        Console.ReadKey();
+        return;
+    }
+
+    /// <summary>
+    /// Concatenates two strings (a[...] and b[...]) and stores the result in c[]
+    /// </summary>
+    /// <param name="a">Input string</param>
+    /// <param name="b">Input string</param>
+    /// <param name="c">Output string</param>
+    /// <param name="n1">Length of the first array</param>
+    /// <param name="n2">Length of the second array</param>
+    public static void Concatenate(char[] a, char[] b, char[] c, int n1, int n2)
+    {
+        int i;
+        for (i = 0; i < n1; i++)
+        {
+            c[i] = a[i];
+        }
+        for (int j = 0; j < n2; j++)
+        {
+            c[i++] = b[j];
+        }
+    }
+}
---
+++ CSharp/Ropes/Ropes.RopeMethod/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    // Maximum length of a leaf string
+    public static readonly int LEAF_LEN = 2;
+
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("Please specify two strings to concatenate.");
+            return;
+        }
+
+        var a = args[0];
+        var b = args[1];
+        var an1 = a.Length;
+        var bn1 = b.Length;
+
+        Rope ropeA = null;
+        Rope ropeB = null;
+
+        ropeA = CreateRopeStructure(ref ropeA, null, a, 0, an1 - 1);
+        ropeB = CreateRopeStructure(ref ropeB, null, b, 0, bn1 - 1);
+
+        Console.ReadKey();
+    }
+
+    private static Rope CreateRopeStructure(ref Rope node, Rope parent, string a, int l, int r)
+    {
+        Rope temp = new Rope();
+        //temp.Left = temp.Right = null; // redundant...
+        temp.Parent = parent;
+        if ((r - l) > LEAF_LEN)
+        {
+            temp.Str = null;
+            temp.LCount = (int)Math.Floor((r - l) / 2.0);
+            node = temp;
+            int m = (int)Math.Floor((l + r) / 2.0);
+            node.Left = CreateRopeStructure(ref node.Left, node, a, l, m);
+            node.Right = CreateRopeStructure(ref node.Right, node, a, m + 1, r);
+        }
+        else
+        {
+            node = temp;
+            temp.LCount = (r - l);
+            for (int i = l; i <= r; i++)
+            {
+                temp.Str.Add(a[i]);
+            }
+        }
+
+        return node;
+    }
+}
+
+internal class Rope {
+    public Rope Left;
+    public Rope Right;
+    public Rope Parent;
+    public List<char> Str;
+    public int LCount;
+
+    public Rope()
+    {
+        //Left = null;
+        //Right = null;
+        //Parent = null;
+        Str = new List<char>();
+        //LCount = 0;
+    }
+}
